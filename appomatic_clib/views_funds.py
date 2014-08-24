733fc4901776d43525422d5dbb92356bb5d28742
Egil Moeller <egil.moller@freecode.no>
2014-08-24 03:01:52 +0200

    Support for withdrawal using paypal

--- appomatic_clib/views_funds.py
+++ appomatic_clib/views_funds.py
@@ -4,6 +4,65 @@ import django.shortcuts
 from django.utils.translation import ugettext_lazy as _
 import django.contrib.messages
 import django.contrib.auth.decorators
+from django.conf import settings
+import urllib2
+import urllib
+import json
+
+def paypal_withdraw(user, amount):
+    if amount > user.profile.available_balance:
+        raise Exception("Too little funds available")
+
+    req = {
+        "actionType": "PAY",
+        "cancelUrl": "http://example.com/cancel",
+        "currencyCode": "EUR",
+        "receiverList": [{
+                "receiver": {
+                    "amount": amount,
+                    "email": user.email
+                    },
+                }],
+        "returnUrl": "http://example.com/return",
+        "sender": {
+            "useCredentials": True
+            },
+        "requestEnvelope": {
+            }
+        }
+    req = json.dumps(req)
+
+    transaction = appomatic_clib.models.Transaction(
+        amount = amount,
+        src = user,
+        dst = None,
+        external_type = "paypal",
+        external_data = user.email,
+        log = req + "\n")
+    transaction.save()
+
+
+    httpReq = urllib2.Request(url=settings.PAYPAL_ENDPOINTS['PAY'],
+                              data=req,
+                              headers=settings.PAYPAL_HEADERS)
+    res = json.load(urllib2.urlopen(httpReq))
+
+    if res["responseEnvelope"]["ack"] != "Success":
+        transaction.delete()
+        raise Exception(res["error"]["message"])
+        
+    transaction.amount = res["paymentInfoList"]["paymentInfo"][0]["receiver"]["amount"]
+
+    transaction.tentative = False
+    transaction.log += json.dumps(res) + "\n"
+
+    if res["paymentExecStatus"] != "COMPLETED":
+        transaction.pending = True
+        transaction.save()
+        raise Exception(res["error"]["message"])
+
+    transaction.save()
+    
 
 class FundsForm(django.forms.Form):
     withdraw = django.forms.FloatField(label="Withdraw", required=False)
@@ -18,8 +77,14 @@ def funds(request):
 
         if form.is_valid():
             if 'do_withdraw' in request.POST:
-                django.contrib.messages.add_message(request, django.contrib.messages.INFO, 'Funds withdrawn.')
+                try:
+                    paypal_withdraw(request.user, form.cleaned_data['withdraw'])
+                except Exception, e:
+                    django.contrib.messages.add_message(request, django.contrib.messages.ERROR, unicode(e))
+                else:
+                    django.contrib.messages.add_message(request, django.contrib.messages.INFO, 'Funds withdrawn.')
             elif 'do_add' in request.POST:
+                
                 django.contrib.messages.add_message(request, django.contrib.messages.INFO, 'Funds added.')
 
     return django.shortcuts.render(request, 'appomatic_clib/funds.html', {
